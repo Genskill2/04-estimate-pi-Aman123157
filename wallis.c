200f11a660557cfdd1ff3ba779966d89daa726fe
Aman123157 <84769493+Aman123157@users.noreply.github.com>
2021-06-04 13:08:03 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,18 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int num)
-{
-  float pi,a,p=1.00;
-  int n;
-  for(n=1;n<=num;n++)
-  {
-    a=4*n*n;
-    p=p*(a/(a-1));
-  }
-  return(pi=p*2.0);
-
-}
 
 int main(void) {
   float pi;
@@ -34,4 +22,57 @@ int main(void) {
     }
   }
 }
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
+
+int main(void) {
+  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+
+    
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+  }
+}
+
+float wallis_pi(int num)
+{
+  float pi,a,p=1.00;
+  int n;
+  for(n=1;n<=num;n++)
+  {
+    a=4*n*n;
+    p=p*(a/(a-1));
+  }
+  return pi=p*2;
+
+}
+
 
